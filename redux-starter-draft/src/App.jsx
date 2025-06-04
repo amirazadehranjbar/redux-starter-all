f181a9cf3686795bc997dc878b2109a66e6e3ae2
amirazadehranjbar <amirazadehranjbar@gmail.com>
2025-06-04 10:52:54 -0700

    add store

--- redux-starter-draft/src/App.jsx
+++ redux-starter-draft/src/App.jsx
@@ -1,9 +1,18 @@
 // src/App.jsx
+import { useDispatch } from "react-redux";
+import { addBug } from "./redux/features/bugs/bugSlice";
 
 const App = ()=>{
+
+    const dispatch = useDispatch();
+
+    const handleAddBug = ()=>{
+        dispatch(addBug({bugDescription:"add bug manual" , userID:"12212"}))
+    }
+
     return(
         <div>
-
+            <button onClick={handleAddBug} className="bg-slate-900 text-slate-300">add bug</button>
         </div>
     )
 }
--- redux-starter-draft/src/redux/features/bugs/bugSlice.js
+++ redux-starter-draft/src/redux/features/bugs/bugSlice.js
@@ -1,25 +1,67 @@
 // src/redux/features/bugs/bugSlice.js
 
-import {createSlice} from "@reduxjs/toolkit";
+import {createSlice, createAsyncThunk} from "@reduxjs/toolkit";
+import axios from "axios";
 
+// post new bug
+const postBug = createAsyncThunk("bugs/postBug", async (bugData, thunkAPI) => {
+
+    try {
+        const response = await axios.post("http://localhost:8000/api/bugs", bugData);
+        return response.data;
+    } catch (e) {
+        return thunkAPI.rejectWithValue(e.response.data);
+    }
+
+});
+
+// bug Slice
 const bugSlice = createSlice({
 
-    name:"bugs",
-    initialState:{
-        list:[],
-        isSolved:false,
-        lastFetch:null,
-    },
-    reducers:{
+    name: "bugs",
 
-        addBug:(bugs , action)=>{
-            bugs.list.push({
-                bugDescription:action.payload.bugDescription
+    initialState: {
+        list: [],
+        isSolved: false,
+        lastFetch: null,
+        isLoading: false,
+        error: null,
+    },
+
+    reducers: {
+
+        // 🧱🧱🧱🧱🧱🧱🧱🧱🧱🧱🧱🧱🧱🧱🧱🧱
+        addBug: (bug, action) => {
+            bug.list.push({
+                bugDescription: action.payload.bugDescription,
+                userID: action.payload.userID,
             })
         },
+
+        // 🧱🧱🧱🧱🧱🧱🧱🧱🧱🧱🧱🧱🧱🧱🧱🧱
+
+    },
+
+    extraReducers: {
+
+        [postBug.pending]: (bug) => {
+            bug.isLoading = true;
+            bug.error = null;
+        },
+
+        [postBug.fulfilled]: (bug, action) => {
+            bug.isLoading = false;
+            bug.list.push(action.payload.data);
+        },
+
+        [postBug.rejected]:(bug , action)=>{
+            bug.isLoading = false;
+            bug.error = action.payload?.error || "some error in post new bug";
+        }
 
     }
 
 });
 
 export const {addBug} = bugSlice.actions;
+export default bugSlice.reducer;
