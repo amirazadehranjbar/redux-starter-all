6820da87f467a3deb3ff7d31e077da7a0b93a473
amirazadehranjbar <amirazadehranjbar@gmail.com>
2025-06-08 16:30:00 -0700

    complete toggle dark and light

--- redux-starter-draft/src/layout.jsx
+++ redux-starter-draft/src/layout.jsx
@@ -1,18 +1,40 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+
 
 // eslint-disable-next-line react/prop-types
 const Layout = ({ children }) => {
+
+    const [isDark , setIsDark] = useState(false);
+
+    const setIsDarkHandler = ()=>{
+        setIsDark(!isDark);
+    }
+
+    useEffect(() => {
+
+        if(isDark){
+            document.documentElement.classList.add('dark');
+        }else{
+            document.documentElement.classList.remove('dark');
+        }
+    }, [isDark]);
+
     return (
         <div className="min-h-screen flex flex-col">
-            <header className="bg-blue-800 text-white p-4">
+            <header className="bg-slate-400 dark:bg-slate-900 text-white p-4 flex flex-row h-[calc(100vh/12)]">
                 <h1>My App Navbar</h1>
+                <button className="bg-slate-800 px-5 py-2 right-5 fixed top-[calc(100vh/12)/2]
+                rounded-md text-slate-400 font-bold hover:scale-110 cursor-pointer" onClick={setIsDarkHandler}>
+                    {isDark ? "Light" : "Dark"}
+                </button>
+
             </header>
 
-            <main className="flex-1 flex items-center justify-center p-6 bg-slate-100">
+            <main className={`flex-1 flex items-center justify-center p-6 ${isDark ? 'bg-gray-800 text-slate-400' : 'bg-slate-100'}`}>
                 {children}
             </main>
 
-            <footer className="bg-blue-900 text-white p-4 text-center">
+            <footer className="bg-slate-400 dark:bg-slate-900 text-white p-4 text-center">
                 © 2025 My App
             </footer>
         </div>
