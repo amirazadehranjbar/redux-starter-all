fb58d6fab1d6159d9ccf2844cdbe0bcc2d1b5307
amirazadehranjbar <amirazadehranjbar@gmail.com>
2025-06-09 18:35:44 -0700

    create user slice

--- backend-draft/routes/userRoutes.js
+++ backend-draft/routes/userRoutes.js
@@ -1,34 +1,44 @@
 // routes/userRoutes.js
-const {Router} = require("express");
+const { Router } = require("express");
 const userRoutes = Router();
 const UserModel = require("../models/userModel");
 
 // new user add
-userRoutes.post("/api/users" , async (req,res)=>{
+userRoutes.post("/api/users", async (req, res) => {
 
-    const {userName , email} = req.body;
+    const { userName, email } = req.body;
 
-    if(!userName) return new Error("user name required !");
-    if(!email) return new Error("email required !");
+    if (!userName) return new Error("user name required !");
+    if (!email) return new Error("email required !");
 
     try {
 
-        const newUser =  new UserModel({
+        const newUser = new UserModel({
             userName,
             email,
         });
 
         await newUser.save();
 
-        return res.status(201).json({message:"success" , data:newUser});
+        return res.status(201).json({ message: "success", data: newUser });
 
 
-    }catch (error){
+    } catch (error) {
 
-        return res.status(401).json({message:"error" , data:error.message})
+        return res.status(401).json({ message: "error", data: error.message })
 
     }
 
+});
+
+// get all users
+userRoutes.get("/api/users", async (req, res) => {
+    try {
+        const users = await UserModel.find();
+        return res.status(200).json({ message: "success", data: users });
+    } catch (error) {
+        return res.status(400).json({ message: "error", data: error });
+    }
 });
 
 
---
+++ redux-starter-draft/src/redux/features/user/userSlice.js
@@ -0,0 +1,89 @@
+import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
+import axios from "axios";
+
+// post new user
+export const postNewUser = createAsyncThunk(
+    'users/postNewUser',async (newUserData, thunkAPI) => {
+        try{
+
+            const response = await axios.post("http://localhost:8000/api/users" , newUserData);
+            return response.data;
+
+        }catch (e) {
+            return thunkAPI.rejectWithValue(e.message);
+        }
+    }
+);
+
+// get all users
+export const getAllUsers = createAsyncThunk(
+    'users/getAllUsers',
+    async (users , thunkAPI)=>{
+
+        try {
+
+            const response = await axios.get("http://localhost:8000/api/users");
+            return response.data;
+
+        }catch (e) {
+            return thunkAPI.rejectWithValue(e);
+        }
+
+    }
+);
+
+
+const userSlice = createSlice({
+    name:"users",
+    initialState:{
+        usersList:[],
+        isPosting:false,
+        isLoading:false,
+        isLoggedIn:false,
+        error:null,
+    },
+    extraReducers:builder => builder
+
+        //region add user *************************************************
+        .addCase(postNewUser.pending , users => {
+            users.isPosting=true;
+            users.error=null;
+        })
+
+        .addCase(postNewUser.fulfilled , (users, action)=>{
+            users.usersList.push(action.payload.newUser);
+            users.isPosting=false;
+            users.isLoggedIn = true;
+        })
+
+        .addCase(postNewUser.rejected , (users , action)=>{
+            users.error = action.payload.error;
+        })
+        //endregion
+
+        //region get all users *****************************************
+        .addCase(getAllUsers.pending,(users)=>{
+            users.isLoading=true;
+            users.error = null;
+        })
+
+        .addCase(getAllUsers.fulfilled , (users , action)=>{
+            users.list = action.payload.data;
+            users.isLoading=false;
+        })
+
+        .addCase(getAllUsers.rejected , (users , action)=>{
+            users.isLoading = false;
+            users.isPosting = false;
+            users.error = action.payload.error;
+
+        })
+        //endregion
+});
+
+
+// export
+export const actions = userSlice.actions;
+export default userSlice.reducer;
+
+
